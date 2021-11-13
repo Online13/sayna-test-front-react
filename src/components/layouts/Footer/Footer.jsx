7b1492c925f3f133a2a084969cca52d57957dc01
RATIARIVELO Nekena Rayane <rratiarivelo@gmail.com>
2021-11-13 10:20:52 +0300

    add footer

---
+++ src/components/layouts/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-grid row">
+        <div className="col-12 col-lg-3">
+          <h3>ADDRESS</h3>
+          <p>
+            400-401 West Georgia Street <br />
+            Vancouver, BC, Canada, V6B 5A1 <br />
+            +1 (123) 456 7890 <br />
+            sales@insurance.com <br />
+          </p>
+        </div>
+        <div className="col-12 col-lg-3">
+          <h3>FOLLOW US</h3>
+          <div className="follow">
+            <Link to="#">Twitter</Link>
+            <Link to="#">Facebook</Link>
+            <Link to="#">LinkedIn</Link>
+            <Link to="#">Instagram</Link>
+          </div>
+        </div>
+        <div className="col-12 col-lg-3">
+          <h3>ABOUT US</h3>
+          <p>
+          Compellingly myocardinate market-driven infrastructures before team driven manufactured products. Monotonectally exploit tactical markets vis-a-vis excellent deliverables.
+          </p>
+        </div>
+      </div>
+      <p>Copyright © 2019. Insurance LLC</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/pages/Main/Main.jsx
+++ src/components/pages/Main/Main.jsx
@@ -1,3 +1,4 @@
+import Footer from "../../layouts/Footer/Footer";
 import Header from "../../layouts/Header/Header";
 import "./Main.scss";
 
@@ -5,6 +6,10 @@ function Main() {
   return (
     <main className="main">
       <Header />
+      <p className="p-2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum voluptas fuga voluptatum vitae molestias. Laboriosam architecto vel tenetur eveniet quasi ratione pariatur, blanditiis voluptatem quam voluptas labore, consequatur voluptate voluptatibus.
+      </p>
+      <Footer />
     </main>
   );
 }
